67e98d07a8a6160aa3a13b49a9b9e65660e2e42d
XSP <73850477+EECS183-XSP@users.noreply.github.com>
2021-09-30 13:36:12 -0400

    Update 1
    // initial code for the function submit_order the class Stall //

--- hw4.py
+++ hw4.py
@@ -1,4 +1,5 @@
 
+
 import unittest
 
 # The Customer class
@@ -27,8 +28,9 @@ class Customer:
     
     # Submit_order takes a cashier, a stall and an amount as parameters, 
     # it deducts the amount from the customer’s wallet and calls the receive_payment method on the cashier object
-    def submit_order(self, cashier, stall, amount): 
-        pass
+    def submit_order(self, cashier, stall, amount):
+        self.wallet -= amount
+        cashier.receive_payment(stall,amount) 
 
     # The __str__ method prints the customer's information.    
     def __str__(self):
@@ -71,9 +73,42 @@ class Cashier:
 
 ## Complete the Stall class here following the instructions in HW_4_instructions_rubric
 class Stall:
-    
-    pass
 
+    def __init__(self, name ,inventory = {}, cost = 7, earnings = 0):
+        self.name = name 
+        self.inventory = inventory 
+        self.cost = cost 
+        self.earnings = earnings 
+
+    def process_order(self, name, quantity):
+        amount = self.inventory[name]
+        if quantity >= amount: 
+            self.inventory[name] = self.inventory[name]- quantity 
+        else:
+          pass
+
+    def has_item(self,name,quantity):
+        if name in self.inventory:
+            food_amount = self.inventory[name]
+            if food_amount >= quantity: 
+                return True 
+            else: 
+                return False 
+        else: 
+            return False 
+
+    def stock_up(self,name,quantity):
+        if name in self.inventory: 
+            self.inventory[name] = self.inventory[name] + quantity 
+        else: 
+            self.inventory[name] = quantity 
+
+    def compute_cost(self,quantity):
+        total = (self.cost) * (quantity)
+        return total 
+
+    def __str__(self):
+        print("Hello, we are", self.name,". THis is the correct menu",self.inventory.keys(),'.We charge $', self.cost, 'per item. We have $'.self.earnings, 'in total.')
 
 class TestAllMethods(unittest.TestCase):
     
